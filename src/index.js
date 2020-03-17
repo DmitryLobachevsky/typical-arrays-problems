a80be0d55b7d2eb4799bd40287d6b87bae36db0d
DmitryLobachevsky <47788358+DmitryLobachevsky@users.noreply.github.com>
2020-03-17 15:45:11 +0300

    feat: solved

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
 
 exports.min = function min (array) {
+  if(array == undefined)
   return 0;
+  let min = Math.min.apply(null ,array);
+  if(min == Infinity)
+  return 0;
+  return min;
 }
 
 exports.max = function max (array) {
+  if(array == undefined)
   return 0;
+  let max = Math.max.apply(null, array);
+  if(max == -Infinity)
+  return 0;
+  return max;
 }
 
 exports.avg = function avg (array) {
+  if(array == undefined)
   return 0;
+  let k = 0;
+  for(let i = 0; i < array.length; i++ )
+    k += array[i];
+  let avg = k/array.length;
+  if(Number.isNaN(k/array.length))
+  return 0;
+  return avg;
 }
